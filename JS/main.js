4ae11d2bbef8d139e9ca4d673cd758c9ba20c5e4
johnhealycodes <johndhealy1216@gmail.com>
2023-03-17 22:43:48 -0500

    Added image assets and additional API calls
    Accessed battle.net WoW token API for US and EU servers. Sorted out layout and added some styling

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,29 @@
+//This is US client
+
 fetch(`https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&locale=en_US&access_token=USVK27cOuge3SyzzzFuIsHEEioQd3MvjCX
 `)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        document.querySelector('h4').innerText = data.price
-        document.querySelector('h5').innerText = data.last_updated_timestamp
-        document.querySelector('p').innerText = data._links.self.href
+        // let token = data.price / 10000
+        document.querySelector('.US').innerText = data.price / 10000
+        document.querySelector('.usAPILink').innerText = data._links.self.href
     })
+    .catch(err => {
+        console.log(`Error ${err}`)
+    })
+
+
+//This is EU Client
+
+fetch(`https://eu.api.blizzard.com/data/wow/token/index?namespace=dynamic-eu&locale=eu_US&access_token=USVK27cOuge3SyzzzFuIsHEEioQd3MvjCX`)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    let token = data.price / 10000
+    document.querySelector('.EU').innerText = data.price / 10000
+    document.querySelector('.euAPILink').innerText = data._links.self.href
+})
+.catch(err => {
+    console.log(`Error ${err}`)
+})
