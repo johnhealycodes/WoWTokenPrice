9e4b50a3b0cc5b20b7458afe2d29b9dc1fce570a
johnhealycodes <johndhealy1216@gmail.com>
2023-03-23 16:47:43 -0500

    Updated fetch info for Blizz API

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 //This is US client
 
-fetch(`https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&locale=en_US&access_token=USY289qZjBTiuZSNWz8TfayIMcmsoiHIZ0`)
+fetch(`https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&locale=en_US&access_token=USyov35v2CcBo0gBRqiOdTTctVVMrI2ps2`)
     .then(res => res.json())
     .then(data => {
-        document.querySelector('.US').innerText = data.price / 10000
+        document.querySelector('.US').innerText = `${data.price / 10000} g`
         document.querySelector('.usAPILink').innerText = data._links.self.href
     })
     .catch(err => {
@@ -13,10 +13,10 @@ fetch(`https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us&loc
 
 //This is EU Client
 
-fetch(`https://eu.api.blizzard.com/data/wow/token/index?namespace=dynamic-eu&locale=en_EU&access_token=USY289qZjBTiuZSNWz8TfayIMcmsoiHIZ0`)
+fetch(`https://eu.api.blizzard.com/data/wow/token/index?namespace=dynamic-eu&locale=en_US&access_token=USyov35v2CcBo0gBRqiOdTTctVVMrI2ps2`)
 .then(res => res.json())
 .then(data => {
-    document.querySelector('.EU').innerText = data.price / 10000
+    document.querySelector('.EU').innerText = `${data.price / 10000} g`
     document.querySelector('.euAPILink').innerText = data._links.self.href
 })
 .catch(err => {
